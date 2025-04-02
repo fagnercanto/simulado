f296bb2b58fe0cb923070fe9c2ab74136d777aee
Fagner Canto <fagnercantosantos@gmail.com>
2025-04-02 07:14:42 -0300

    Últimos ajustes antes do teste

---
+++ SimuladoApi.Tests/PessoaTests.cs
@@ -0,0 +1,11 @@
+﻿namespace SimuladoApi.Tests;
+using Xunit;
+public class PessoaTests
+{
+    [Fact]
+    public void CriarPessoa_DeveRetornarObjetoComNome()
+    {
+        var pessoa = new Pessoa { Nome = "Fagner" };
+        Assert.Equal("Fagner", pessoa.Nome);
+    }
+}
